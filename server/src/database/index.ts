505915b443dfcf3c1ff908078b87d31f75dbe6db
Katie Lamber <katielamber02@gmail.com>
2020-01-31 15:51:45 +0200

    server and basic client setup

---
+++ server/src/database/index.ts
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+import { Database } from "../lib/types";
+
+// const url = `mongodb+srv://${process.env.DB_USER}:${
+//   process.env.DB_USER_PASSWORD
+// }@${process.env.DB_CLUSTER}.mongodb.net`;
+
+
+
+
+export const connectDatabase = async (): Promise<Database> => {
+  const client = await MongoClient.connect(
+    // mongodb://<dbuser>:<dbpassword>@ds215219.mlab.com:15219/test_listings
+    // INSERT DB URL
+    ,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
+  const db = client.db("test_listings");
+ 
+
+  return {
+    listings: db.collection("test_listings")
+  };
+};
